aef76d94e7ed4c93d89052976161ac9316505ea3
Neil Satra <neilsatra@194.71.111.64.naturalwireless.com>
2023-05-12 17:48:18 -0400

    Add login link or current user info to header based on auth status

--- web/src/components/TopNav/TopNav.tsx
+++ web/src/components/TopNav/TopNav.tsx
@@ -1,27 +1,35 @@
-import { NavLink, routes } from "@redwoodjs/router"
+import { Link, NavLink, routes } from "@redwoodjs/router"
+import { useAuth } from "src/auth"
 
 const TopNav = () => {
+  const { isAuthenticated, currentUser, logOut } = useAuth()
+
   return (
     <header className="rw-header">
-    <NavLink to={routes.messages()} className="rw-link" activeClassName="rw-heading rw-heading-primary">
-      Messages
-    </NavLink>
-    <NavLink to={routes.citations()} className="rw-link" activeClassName="rw-heading rw-heading-primary">
-      Citations
-    </NavLink>
-    <NavLink to={routes.citizens()} className="rw-link" activeClassName="rw-heading rw-heading-primary">
-      Citizens
-    </NavLink>
-    <NavLink to={routes.home()} className="rw-link" activeClassName="rw-heading rw-heading-primary">
-      Home
-    </NavLink> 
-    <NavLink to={routes.login()} className="rw-link" activeClassName="rw-heading rw-heading-primary">
-      Login
-    </NavLink>
-    <NavLink to={routes.signup()} className="rw-link" activeClassName="rw-heading rw-heading-primary">
-      Sign up
-    </NavLink>
-  </header>
+      <NavLink to={routes.messages()} className="rw-link" activeClassName="rw-heading rw-heading-primary">
+        Messages
+      </NavLink>
+      <NavLink to={routes.citations()} className="rw-link" activeClassName="rw-heading rw-heading-primary">
+        Citations
+      </NavLink>
+      <NavLink to={routes.citizens()} className="rw-link" activeClassName="rw-heading rw-heading-primary">
+        Citizens
+      </NavLink>
+      <NavLink to={routes.home()} className="rw-link" activeClassName="rw-heading rw-heading-primary">
+        Home
+      </NavLink>
+
+      {isAuthenticated ? (
+        <div>
+          <span>Logged in as {currentUser.name}</span>{' '}
+          <button type="button" onClick={logOut}>
+            Logout
+          </button>
+        </div>
+      ) : (
+        <Link to={routes.login()}>Login</Link>
+      )}
+    </header>
   )
 }
 
